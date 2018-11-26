098c5411cac31398ffa4b9cff9086ce6c036e408
Vytautas Perliba <vperliba@kayak.com>
2018-11-26 17:04:18 +0200

    relace cards with material cards

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -3,63 +3,49 @@ import { withTheme } from '@material-ui/core/styles';
 import DeleteIcon from '@material-ui/icons/Delete';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import NotFavoriteIcon from '@material-ui/icons/FavoriteBorder';
-import Modal from '@material-ui/core/Modal';
-import Button from '@material-ui/core/Button';
+import MaterialCard from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
 
 import styles from './card.module.scss';
 
-class Card extends React.Component {
-  state = {
-    isDescriptionOpen: false,
-  };
-
-  openDescription = () => this.setState({ isDescriptionOpen: true });
-  closeDescription = () => this.setState({ isDescriptionOpen: false });
-
-  deleteCard = (e) => {
-    e.stopPropagation();
-    this.props.onDelete();
-  };
-
-  likeCard = (e) => {
-    e.stopPropagation();
-    this.props.toggleLike();
-  }
-
-  render() {
-    const { imageSrc, title, description, isLiked, isMine, likesCount } = this.props;
-    const imageStyle = { backgroundImage: `url(${imageSrc})`};
-    return (
-      <React.Fragment>
-        <div className={styles.card} onClick={this.openDescription}>
-          <div className={styles.overlay} />
-          <div className={styles.imageContainer}>
-            <div className={styles.image} style={imageStyle} />
-          </div>
-          <div className={styles.likes} onClick={this.likeCard}>
-            {isLiked ? <FavoriteIcon className={likesCount > 0 ? styles.liked : ''} /> : <NotFavoriteIcon className={likesCount > 0 ? styles.liked : ''} />} 
-            <span>{likesCount}</span>
-          </div>
-          <div className={styles.footer}>
-            <div className={styles.title}>{title}</div>
-            {isMine && <DeleteIcon className={styles.delete} onClick={this.deleteCard} />}
-          </div>
-        </div>
-        <Modal open={this.state.isDescriptionOpen} onClose={this.closeDescription}>
-          <div className={styles.modal}>
-            <div className={styles.description}>
-              {description}
-            </div>
-            <div className={styles.close}>
-              <Button variant="outlined" className={styles.button} onClick={this.closeDescription}>
-                Close
-              </Button>
-            </div>
-          </div>
-        </Modal>
-      </React.Fragment>
-    );
-  }
-}
+const Card = ({
+  imageSrc,
+  title,
+  description,
+  isLiked,
+  isMine,
+  likesCount,
+  onDelete,
+  toggleLike
+}) => (
+  <MaterialCard className={styles.card}>
+    <CardMedia className={styles.media} image={imageSrc} title="Paella dish" />
+    <CardContent className={styles.content}>
+      <Typography gutterBottom variant="h5" component="h2">
+        {title}
+      </Typography>
+      <Typography component="p">{description}</Typography>
+    </CardContent>
+    <CardActions className={styles.actions} disableActionSpacing>
+      <IconButton onClick={toggleLike}>
+        {isLiked ? (
+          <FavoriteIcon className={likesCount > 0 ? styles.liked : ''} />
+        ) : (
+          <NotFavoriteIcon className={likesCount > 0 ? styles.liked : ''} />
+        )}
+      </IconButton>
+      <Typography component="p">{likesCount}</Typography>
+      {isMine && (
+        <IconButton onClick={onDelete} className={styles.delete}>
+          <DeleteIcon />
+        </IconButton>
+      )}
+    </CardActions>
+  </MaterialCard>
+);
 
 export default withTheme()(Card);
